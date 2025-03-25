b8964333c1825b5bd6383d14c876deb0a7dccf62
Aman Singh <amansinghjiit@gmail.com>
2025-03-25 14:23:45 +0530

    allowed hosts changed

--- TrackMatch/settings.py
+++ TrackMatch/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["www.creditrade.in", "creditrade.in"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
