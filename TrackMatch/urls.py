1617169265bf06a12b30144347fca886d4529157
Aman Singh <amansinghjiit@gmail.com>
2025-05-03 11:04:46 +0530

    updated url

--- TrackMatch/urls.py
+++ TrackMatch/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path
 from .views import AsyncScraperView
 
+def home_view(request):
+    return JsonResponse({"status": "API running"})
+
 urlpatterns = [
+    path('', home_view),
     path('admin/', admin.site.urls),
     path('api/', AsyncScraperView.as_view(), name='api')
 ]
